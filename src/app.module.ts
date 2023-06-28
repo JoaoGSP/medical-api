ae96aa4c8ebe2571ef2315ba37267ede7a339da6
João GS Pereira <63313287+JoaoGSP@users.noreply.github.com>
2023-06-28 11:59:08 -0300

    FEAT:sparkles: Add mongo connection
    FEAT:sparkles: Add Swagger setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { MongooseModule } from '@nestjs/mongoose'
+
+const user = process.env.DB_USER
+const password = process.env.DB_PASS
+const host = process.env.DB_HOST
+const database = process.env.DB_NAME
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://${user}:${password}@${host}/${database}?retryWrites=true&w=majority`
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
