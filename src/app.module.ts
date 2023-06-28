6e6d4ca81f0b202450c95ca0597329bec3e154da
João GS Pereira <63313287+JoaoGSP@users.noreply.github.com>
2023-06-28 12:22:19 -0300

    :fire: refactor: Remove the profile document and replaced by user document
    :sparkles: feat: Create the user schema to be the base document on the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
+import { AttendantModule } from './attendant/attendant.module'
+import { ClientModule } from './client/client.module'
+import { ConsultationModule } from './consultation/consultation.module'
+import { DoctorModule } from './doctor/doctor.module'
+import { LoginModule } from './login/login.module'
+import { UserModule } from './user/user.module'
 
 const user = process.env.DB_USER
 const password = process.env.DB_PASS
@@ -13,6 +19,12 @@ const database = process.env.DB_NAME
     MongooseModule.forRoot(
       `mongodb+srv://${user}:${password}@${host}/${database}?retryWrites=true&w=majority`
     ),
+    AttendantModule,
+    ClientModule,
+    ConsultationModule,
+    DoctorModule,
+    LoginModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
